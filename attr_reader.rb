57f44d310eb52fc4408aba3818956ad2e8cd40ca
victor-akinboro <victorakinboro2003@gmail.com>
2023-06-07 18:10:21 +0100

    attr_reader

--- attr_reader.rb
+++ attr_reader.rb
@@ -1,22 +1,24 @@
 class Student 
+  attr_reader :name, :email, :location
   def initialize(name, email, location)
     @name = name
     @email = email
     @location = location
   end
 
-  def name
-    @name #ruby returns the last line automatically
+  def name=(name)
+    @name = name #ruby returns the last line automatically
   end
 
-  def email
-    @email
+  def email=(email)
+    @email = email
   end
 
-  def location
-    @location
+  def location=(location)
+    @location = location
   end
 end
 
 schoolOne = Student.new('monument', 'monument@gmail.com', 'Canada')
+# schoolOne.location("New york")
 puts schoolOne.location()
